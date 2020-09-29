0f83da0d96694a903e21002be53a2f418cace9c0
Pirogov Evgenij <pirogovevgenij@gmail.com>
2020-09-29 09:59:14 +0300

    feat(hooks/http): add logout when token expired

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -1,8 +1,12 @@
-import {useCallback, useState} from 'react'
+import {useCallback, useContext, useState} from 'react'
+import {AuthContext} from "../context/AuthContext";
+
 
 export const useHttp = () => {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const auth = useContext(AuthContext)
+
 
   const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
     setLoading(true)
@@ -16,7 +20,10 @@ export const useHttp = () => {
       const response = await fetch(url, {method, body, headers})
       const data = await response.json()
 
-      if (response.status === 401) throw new Error('Wrong username or password')
+      if (response.status === 401) {
+        auth.logout()
+        throw new Error('Wrong username or password')
+      }
       if (response.status === 403) throw new Error('Доступ запрещен')
       if (!response.ok) throw new Error( data.message || 'Что-то пошло не так')
 
